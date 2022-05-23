670e26099afef1b589507cb686bc898b308e9f52
katyaynipandey <106030020+katyaynipandey@users.noreply.github.com>
2022-05-23 22:33:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+def add (a,b):
+    return a+b
+def subtract (a,b):
+    return a-b
+def multiply (a,b):
+    return a*b
+def divide (a,b):
+    return a/b 
+
+print("Select opertation")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True :
+    choice = input("Enter choice (1/2/3/4): ")
+    if choice in ('1','2','3','4'):
+        a=float(input("Enter the first number: "))
+        b=float(input("Enter the second number: "))
+        if choice == '1':
+            print(a,"+",b,"=", add(a,b))
+        elif choice == '2':
+            print(a,"-",b,"=", subtract(a,b))
+        elif choice == '3':
+            print(a,"*",b,"=", multiply(a,b))
+        elif choice == '4':
+            print(a,"/",b,"=",divide(a,b))
+        break
+    else: 
+        print("Invalid input")
+            
